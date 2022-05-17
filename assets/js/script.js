7352a086589e6eb8f04e33935906a53f576ab26e
Jason L <jkleitz27@gmail.com>
2022-05-17 12:47:38 -0400

    fixed js

--- assets/js/script.js
+++ assets/js/script.js
@@ -79,8 +79,4 @@ var getApi =function(data) {
 
 userFormEl.addEventListener("submit", formSubmitHandler);
 fetchButton.addEventListener('click', getApi);
-<<<<<<< HEAD
 //getApi();
-=======
-//getApi();
->>>>>>> af0a1353f74adfc4c708bb86b0003a8bdacd2005
